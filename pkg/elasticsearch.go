62fdb171a7da901642839ce244fba94cedadfeb3
Sidharth <siddarthsuvarna99@gmail.com>
2025-04-01 01:17:49 +0530

    added es wrapper for interface substitution

---
+++ pkg/elasticsearch.go
@@ -0,0 +1,62 @@
+package pkg
+
+import (
+	"context"
+	"io"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+)
+
+// ElasticsearchClient defines an interface for Elasticsearch operations
+type ElasticsearchClient interface {
+	// Index indexes a document in Elasticsearch
+	Index(index string, body io.Reader) (*esapi.Response, error)
+
+	// Info returns info about the cluster
+	Info() (*esapi.Response, error)
+}
+
+// ElasticsearchWrapper wraps the elasticsearch client to implement the ElasticsearchClient interface
+type ElasticsearchWrapper struct {
+	client *elasticsearch.Client
+}
+
+// NewElasticsearchClient creates a new Elasticsearch client wrapper
+func NewElasticsearchClient(config elasticsearch.Config) (ElasticsearchClient, error) {
+	client, err := elasticsearch.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ElasticsearchWrapper{
+		client: client,
+	}, nil
+}
+
+// Index indexes a document in Elasticsearch
+func (e *ElasticsearchWrapper) Index(index string, body io.Reader) (*esapi.Response, error) {
+	return e.client.Index(index, body)
+}
+
+// Search performs a search query
+func (e *ElasticsearchWrapper) Search(indices []string, body io.Reader) (*esapi.Response, error) {
+	return e.client.Search(
+		e.client.Search.WithContext(context.Background()),
+		e.client.Search.WithIndex(indices...),
+		e.client.Search.WithBody(body),
+	)
+}
+
+// Get retrieves a document by ID
+func (e *ElasticsearchWrapper) Get(index, id string) (*esapi.Response, error) {
+	return e.client.Get(
+		index,
+		id,
+	)
+}
+
+// Info returns info about the cluster
+func (e *ElasticsearchWrapper) Info() (*esapi.Response, error) {
+	return e.client.Info()
+}
--- pkg/go.mod
+++ pkg/go.mod
@@ -3,13 +3,20 @@ module github.com/siddarth99/banking-ledger/pkg
 go 1.23.3
 
 require (
+	github.com/elastic/go-elasticsearch/v8 v8.17.1
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/rabbitmq/amqp091-go v1.10.0
 )
 
 require (
+	github.com/elastic/elastic-transport-go/v8 v8.6.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
